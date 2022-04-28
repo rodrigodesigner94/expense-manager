e309e189db467e8fc1b2fec52aa0b4b3025ca009
Rodrigo Borges <digodesign94@gmail.com>
2022-04-27 22:52:28 -0300

    primery commit

---
+++ src/App.styles.ts
@@ -0,0 +1,26 @@
+import styled from 'styled-components';
+
+export const Container = styled.div`
+
+`;
+export const Header = styled.div`
+background-color: darkblue;
+height: 150px;
+text-align: center;
+`;
+
+export const HeaderText = styled.div`
+margin: 0;
+padding: 0;
+color: #fff;
+padding-top: 30px;
+font-family: 'Franklin Gothic Medium';
+font-size: large;
+
+`;
+
+export const Body = styled.div`
+margin: auto;
+max-width: 980px;
+margin-bottom: 50px;
+`;
